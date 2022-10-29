72bb456d1e54e4477dac21a993dff8455fe94b17
Titus Kiplagat <tituskiplagat210@gmail.com>
2022-10-29 22:21:40 +0300

    grocery web

---
+++ grocery Website/javascript/script.js
@@ -0,0 +1,7 @@
+let cart = document.querySelector("#cart-btn");
+
+
+cart.addEventListener('click', () => {
+    const cartList = document.querySelector(".checkoutCart");
+    cartList.style.right = "2rem";
+})
