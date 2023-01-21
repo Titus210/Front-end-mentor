f6a162167dd03083a4154665a5db26e54212591e
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-01-21 18:04:06 +0300

    Landing Page

---
+++ News-HomePage/Code/script.js
@@ -0,0 +1,19 @@
+// Get Elements
+var open = document.getElementById("open")
+var close = document.getElementById("close")
+var navBar = document.getElementById("nav-bar")
+
+// confirm if elements works properly
+function checkOpen(){
+   navBar.style.display = "flex";
+   close.style.display = "flex";
+   open.style.display = "none";
+}
+function checkClose(){
+    navBar.style.display = "none";
+    close.style.display = "none";
+    open.style.display = "flex";
+}
+
+open.addEventListener("click", checkOpen);
+close.addEventListener("click", checkClose);
