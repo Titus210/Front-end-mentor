93e1e690e33f73385a3871af3f64b47272def586
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-04-08 06:49:48 +0300

    tailwind configurations' Packate json'

--- Event Management/tailwind.config.js
+++ Event Management/tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [`./*.html`],
+  content: ['./*.html'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        primaryBlue: '#00D6FA',
+        mainWhite: '#ffffff',
+        mainBlack: '#000000',
+        mainOrange: '#FF8C00',
+        lightGrey: '#f5f5f5',
+        darkGrey: '#ececec',
+      },
+      backgroundImage: {
+        heroBackground: "linear-gradient(90deg, rgba(3, 21, 43, 0.6) 0 %, rgba(0, 0, 0, 0) 105.07 %)",
+        heroImage: "url('./Assets/image-1.png')",
+      },
+    }
   },
   plugins: [],
 }
