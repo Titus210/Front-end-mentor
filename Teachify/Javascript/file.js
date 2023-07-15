931e4abd81e95d14a6ec0b1c1910d80309746baa
Titus210 <tituskiplagat210@gmail.com>
2023-07-15 17:27:35 +0300

    feat: TESTIMONIAL SECTION " Designing and implementing of javascript function on frequently asked questions" **HTML**  - created article cards with each faq  ** CSS**  - displayed none when not clicked  **jS**  - toggle open classname to open article cards  - change icons on click (plus/minus)

--- Teachify/Javascript/file.js
+++ Teachify/Javascript/file.js
@@ -3,3 +3,21 @@ window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 20);
 
 })
+
+
+/* Show and hide faq when faq  is clicked*/
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        // change icon when clicked
+        const icon = faq.querySelector('.faq-icon i');
+        if (icon.className == 'fa fa-plus') {
+            icon.className = 'fa fa-minus'
+        } else {
+            icon.className = 'fa fa-plus'
+        }
+    })
+})
